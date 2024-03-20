7499eeff853da3798ed459693e27e86b93097232
sharma171 <68746747+sharma171@users.noreply.github.com>
2024-03-20 15:28:37 +0530

    blocks add

--- src/component/GrapeJsEditor.js
+++ src/component/GrapeJsEditor.js
@@ -1,9 +1,10 @@
-// GrapeJsEditor.js
 import React, { useEffect, useRef } from 'react';
 import 'grapesjs/dist/css/grapes.min.css';
 import grapesjs from 'grapesjs';
 import "./grapes.css";
 import customBlocks from './DraggableBlock';
+import Prism from 'prismjs'; // Import Prism.js
+import 'prismjs/themes/prism.css'; // Import Prism.js theme CSS file
 
 const GrapeJsEditor = () => {
   const editorRef = useRef(null);
@@ -16,20 +17,28 @@ const GrapeJsEditor = () => {
 
     // Register custom blocks with the Block Manager
     customBlocks.forEach(block => {
-      console.log('Block attributes:', block.attributes);
-      console.log('Block styles:', block.styles);
       editor.BlockManager.add(block.id, {
         label: block.label,
         content: block.content,
         attributes: block.attributes || {}, // Ensure default empty object if attributes are not provided
         category: 'Structure', // Specify the category for the blocks
-        // Optionally, you can define styles for the block
-        styles: block.styles,
+        styles: block.styles, // Optionally, you can define styles for the block
         tagName: block.tagName,
         type: block.type,
         components: block.components,
       });
     });
+
+    // Function to apply Prism.js syntax highlighting
+    const applySyntaxHighlighting = () => {
+      const codeBlocks = editorRef.current.querySelectorAll('pre code');
+      codeBlocks.forEach((codeBlock) => {
+        Prism.highlightElement(codeBlock);
+      });
+    };
+
+    // Call the function to apply syntax highlighting
+    applySyntaxHighlighting();
 
     return () => editor.destroy();
   }, []);
